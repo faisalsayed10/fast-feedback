a9651271d594557193f7204f854fcfd450cf6329
faisalsayed10 <faisal.sayed502@gmail.com>
2020-12-23 14:39:59 +0530

    React 2025 Day - 3

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,48 @@
-import { Button, Code, Heading, Text } from '@chakra-ui/react';
+import { Flex, Button } from '@chakra-ui/react';
 import Head from 'next/head';
 import { useAuth } from '@/lib/auth';
+import { FastFeedbackIcon, GitHubIcon, GoogleIcon } from 'public/icons';
 
 export default function Home() {
   const auth = useAuth();
   const { signinWithGithub, signinWithGoogle } = auth;
 
   return (
-    <div>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
         <title>Fast Feedback</title>
       </Head>
-
-      <main>
-        <Heading>Fast Feedback</Heading>
-        <Text>
-          Current User: <Code>{auth?.user?.email}</Code>
-        </Text>
-        {auth.user ? (
-          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
-        ) : (
-          <>
-            <Button onClick={(e) => signinWithGithub()}>
-              Sign In With Github
-            </Button>
-            <Button onClick={(e) => signinWithGoogle('/')}>
-              Sign In With Google
-            </Button>
-          </>
-        )}
-      </main>
-    </div>
+      <FastFeedbackIcon color="black" boxSize="64px" />
+      {auth.user ? (
+        <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+      ) : (
+        <>
+          <Button
+            mt={4}
+            size="sm"
+            backgroundColor="#EDF2F7"
+            onClick={(e) => signinWithGithub()}
+          >
+            <GitHubIcon mr={2} boxSize="24px" />
+            Sign In With Github
+          </Button>
+          <Button
+            mt={4}
+            backgroundColor="#EDF2F7"
+            size="sm"
+            onClick={(e) => signinWithGoogle('/')}
+          >
+            <GoogleIcon mr={2} boxSize="24px" />
+            Sign In With Google
+          </Button>
+        </>
+      )}
+    </Flex>
   );
 }
