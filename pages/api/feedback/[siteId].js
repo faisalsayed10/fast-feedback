647ff03fe9f79c205bcc6829d337bcdcff9e6482
faisalsayed10 <faisal.sayed502@gmail.com>
2020-12-25 15:52:22 +0530

    React 2025 Day - 5

---
+++ pages/api/feedback/[siteId].js
@@ -0,0 +1,13 @@
+import { getAllFeedback } from '@/lib/db-admin';
+import db from '@/lib/firebase-admin';
+
+export default async (req, res) => {
+  const siteId = req.query.siteId
+  const {feedbacks, error} = await getAllFeedback(siteId);
+
+  if(error) {
+    res.status(500).json({ error });
+  }
+
+  res.status(200).json({ feedbacks })
+}
---
+++ pages/p/[siteId].js
@@ -0,0 +1,80 @@
+import { useRef, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+
+import Feedback from '@/components/Feedback';
+import { useAuth } from '@/lib/auth';
+import { getAllFeedback, getAllSites } from '@/lib/db-admin';
+import { createFeedback } from '@/lib/db'
+
+const SiteFeedback = ({ initialFeedback }) => {
+  const auth = useAuth()
+  const router = useRouter()
+  const inputEl = useRef(null)
+  const [allFeedback, setAllFeedback] = useState(initialFeedback)
+  
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const newFeedback = {
+      author: auth.user.name,
+      authorId: auth.user.uid,
+      siteId: router.query.siteId,
+      text: inputEl.current.value,
+      createdAt: new Date().toISOString(),
+      provider: auth.user.provider,
+      status: 'pending'
+    }
+
+    createFeedback(newFeedback)
+    setAllFeedback([newFeedback, ...allFeedback])
+  }
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="full"
+      maxW="700px"
+      margin="0 auto"
+    >
+      <Box as="form" onSubmit={onSubmit}>
+        <FormControl my={8}>
+          <FormLabel htmlFor="comment">Feedback</FormLabel>
+          <Input ref={inputEl} type="comment" id="comment" />
+          <Button type="submit" mt={2} fontWeight="medium">
+            Add Feedback
+          </Button>
+        </FormControl>
+      </Box>
+      {allFeedback.map((feedback) => (
+        <Feedback key={feedback.id} {...feedback} />
+      ))}
+    </Box>
+  );
+};
+
+export async function getStaticProps(context) {
+  const siteId = context.params.siteId;
+  const {feedbacks} = await getAllFeedback(siteId);
+  return {
+    props: {
+      initialFeedback: feedbacks
+    }
+  };
+}
+
+export async function getStaticPaths() {
+  const {sites} = await getAllSites();
+  const paths = sites.map((site) => ({
+    params: {
+      siteId: site.id.toString()
+    }
+  }));
+  return {
+    paths,
+    fallback: false
+  };
+}
+
+export default SiteFeedback;
